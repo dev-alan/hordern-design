19ad6b756501c2efdfad7e044cac369f1311323b
Alan <alanl@sitesuite.com.au>
2017-10-09 14:09:17 +1100

    owl-carousel added

--- code/src/global.js
+++ code/src/global.js
@@ -1,11 +1,28 @@
 var page = {
   init: function() {
     this.loadBackgrounds();
+    this.loadGalleries();
   },
   loadBackgrounds: function(){
     Array.prototype.slice.call(document.querySelectorAll('[data-background]')).forEach(function(el){
       el.style.backgroundImage = 'url("' + el.getAttribute('data-background') + '")';
     });
+  },
+  loadGalleries: function(){
+    if(document.querySelector('.slideshow')) {
+      $(function(){
+        Array.prototype.slice.call(document.querySelectorAll('.slideshow')).forEach(function(el){
+          $(el).owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            autoplayHoverPause: true,
+            nav: false,
+            dots: true
+          });
+        });
+      });
+    }
   }
 }
 
